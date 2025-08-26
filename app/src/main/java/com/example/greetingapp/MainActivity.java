6e06815940e270b2e493063b7b3a883658310f00
Shreyash Bhardwaj <shreyashbhardwaj2002@gmail.com>
2025-08-26 08:19:27 +0530

    Created the App

--- app/src/main/java/com/example/greetingapp/MainActivity.java
+++ app/src/main/java/com/example/greetingapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.greetingapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,16 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        EditText et = findViewById(R.id.editText);
+        String name = et.getText().toString();
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello "+ et.getText().toString() +"!", Toast.LENGTH_LONG).show();
+            }
         });
     }
 }
